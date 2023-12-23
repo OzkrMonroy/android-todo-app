e234791a8316cdc810f7b48de105b5bdf9ea8626
Oscar Monroy <oscarmonroy657@gmail.com>
2023-12-23 03:18:19 -0600

    feat(TodoDatabase, DatabaseModule, TaskDao, TaskEntity): Setup room database dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,10 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 }
 
 kapt {
